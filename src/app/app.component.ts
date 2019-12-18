06e3fa3bef6f89685927f7ae921346b9fc24db14
John Downie <john@bluestone360.co.uk>
2019-12-18 06:48:17 +0000

    retrieving data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { Car } from './car';
+import { CarService } from './car.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'angular-php';
+export class AppComponent implements OnInit {
+  cars: Car[];
+  error = '';
+  success = '';
+
+  constructor(private carService: CarService) {
+  }
+
+  ngOnInit() {
+    this.getCars();
+  }
+
+  getCars(): void {
+    this.carService.getAll().subscribe(
+      (res: Car[]) => {
+        this.cars = res;
+      },
+      (err) => {
+        this.error = err;
+      }
+    );
+  }
 }
---
+++ src/app/car.service.ts
@@ -0,0 +1,35 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpErrorResponse, HttpParams } from '@angular/common/http';
+
+import { Observable, throwError } from 'rxjs';
+import { map, catchError } from 'rxjs/operators';
+
+import { Car } from './car';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CarService {
+  baseUrl = 'http://angularphp/api';
+  cars: Car[];
+
+  constructor(private http: HttpClient) { }
+
+  getAll(): Observable<Car[]> {
+    return this.http.get(`${this.baseUrl}/list`).pipe(
+      map((res) => {
+        this.cars = res['data'];
+        return this.cars;
+    }),
+    catchError(this.handleError));
+  }
+
+  private handleError(error: HttpErrorResponse) {
+    console.log(error);
+
+    // return an observable with a user friendly message
+    return throwError('Error! something went wrong.');
+  }
+}
+
+
